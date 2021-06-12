2c664ec80b7bb23e7e0a943062a9f98e9e42a034
alisher8 <80209649+alisher8@users.noreply.github.com>
2021-06-13 02:01:43 +0500

    added backend server

---
+++ src/node mongodb/models/student.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: 'This field is required'
+    },
+    password: {
+        type: String,
+        required: 'This field is required'
+    
+    }
+}) 
+
+mongoose.model("Student", userSchema);
